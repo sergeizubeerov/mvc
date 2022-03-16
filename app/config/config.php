34605ff70445d642de898802828cb32624fd4a48
sergeizubeerov <sergeizubeerov@gmail.com>
2022-03-16 22:57:38 +0100

    did smth

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php
+
+// DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'mvc');
+
 //    app root
 define('APPROOT', dirname(dirname(__FILE__)));
 
